fd616e497b52d4ca326c55573d34a9ee52f9a02a
marcin93 <marcin93@gmail.com>
2014-03-04 20:02:09 +0100

    initial metadata section

--- geo_city_parse.py
+++ geo_city_parse.py
@@ -1,14 +1,16 @@
-#! /usr/bin/env python2.7
+#! /usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-#import os
+import os
 import requests
 import xml.etree.ElementTree as ET
 
 txt = open('input.txt', 'r').read().split('\n')
+i = 0;
 
 for line in txt:
-    lp, region, district, municipality, city, x, y = line.split(',')
+    i += 1
+    lp, region, district, municipality, city, lat, lon = line.split('\t')
     baseUrl = 'http://nominatim.openstreetmap.org/search/pl/'+region+'/'+district+'/'+municipality+'/'+city+'/?format=xml'
     resp = requests.get(baseUrl)
     msg = resp.content
@@ -21,3 +23,7 @@ for line in txt:
     f = open('result.txt', 'a')
     f.write(location+'\t'+region+'\t'+district+'\t'+municipality+'\t'+city+'\n')
     f.close()
+
+g = open('stats.txt', 'a')
+g.write(('value of "i" is: ')+str(i))
+g.close()
